e8783a0dd3fd5baff3adc02787663e493f1be04b
jf <22012032+jobf@users.noreply.github.com>
2023-03-15 04:32:30 +0000

    syntax highlight

--- src/FlxShaderToyHack.hx
+++ src/FlxShaderToyHack.hx
@@ -56,18 +56,17 @@ class FlxShaderToyHack extends FlxGraphicsShader {
 			if(useDefaultFunction){
 				/** the default glsl function that shadertoy uses when you make a new one **/
 				mainImageFunction = '
-				void mainImage( out vec4 fragColor, in vec2 fragCoord )
-				{
-					// Normalized pixel coordinates (from 0 to 1)
-					vec2 uv = fragCoord/iResolution.xy;
-					
-					// Time varying pixel color
-					vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
-					
-					// Output to screen
-					fragColor = vec4(col,1.0);
-				}
-				';
+		void mainImage( out vec4 fragColor, in vec2 fragCoord )
+		{
+			// Normalized pixel coordinates (from 0 to 1)
+			vec2 uv = fragCoord/iResolution.xy;
+			
+			// Time varying pixel color
+			vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+			
+			// Output to screen
+			fragColor = vec4(col,1.0);
+		}';
 		}
 		this.void_mainImage = mainImageFunction;
 
